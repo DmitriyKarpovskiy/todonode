ae7e3141743752d0a2191e5a083ed44df583de4c
Dmitriy Karpovskiy <dmkarpovskiy@gmail.com>
2019-10-09 19:43:45 +0300

    mongoDB is added to config

--- app/app.js
+++ app/app.js
@@ -45,4 +45,16 @@ module.exports = app;
 
 http.createServer(app).listen(config.port, function() {
     console.log('Express server listening on port ' + config.port);
-})
+});
+
+const MongoClient = require("mongodb").MongoClient;
+
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true })
+mongoClient.connect(function(err, client) {
+
+    if (err) {
+        return console.log(err);
+    }
+    // взаимодействие с базой данных
+    client.close();
+});
