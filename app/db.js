11c5a8cb6f813966921b19c8942c74f0e57efd52
Dmitriy Karpovskiy <dmkarpovskiy@gmail.com>
2019-10-16 20:07:22 +0300

    DB url connection is corrected. config.json is added to package

--- app/db.js
+++ app/db.js
@@ -6,11 +6,17 @@ const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'todo';
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+//const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url = 'mongodb://127.0.0.1/todo';
+try {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("OK");
+} catch (ex) {
+    throw new Error('Error');
+}
 
-mongoose.connect(url, { useNewUrlParser: true });
 
-mongoose.Promise = global.Promise();
+//mongoose.Promise = global.Promise();
 
 let db = mongoose.connection;
 
