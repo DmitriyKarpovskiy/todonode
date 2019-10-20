e3cf11bacf2b969819826db0338911f365496836
Dmitriy Karpovskiy <dmkarpovskiy@gmail.com>
2019-10-20 17:25:36 +0300

    Authorization html is added partly

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/auth', function(req, res, next) {
+    res.render('authorization');
 });
 
 module.exports = router;
