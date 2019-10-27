cbe8cd35aa78d214e3d4210d2a0f22dab50c9b51
Dmitriy Karpovskiy <dmkarpovskiy@gmail.com>
2019-10-27 13:32:46 +0200

    Registration page was added

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'ToDo Application' });
 });
 
 router.get('/auth', function(req, res, next) {
     res.render('authorization');
+});
+
+router.get('/registration', function(req, res, next) {
+    res.render('registration');
 });
 
 module.exports = router;
